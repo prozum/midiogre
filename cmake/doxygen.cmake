48be5db598af5f6ca3a0d54f28956f1a29378cee
Niclas Moeslund Overby <nimoov@prozum.dk>
2014-11-02 12:16:46 +0100

    * Make CMake pretty (Unsuccessful)

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -1,20 +1,18 @@
-if (DOC)
-    find_package(Doxygen)
+find_package(Doxygen)
 
-    if(NOT DOXYGEN_FOUND)
-        	message(FATAL_ERROR "Doxygen not found!")
-    endif()
-
-    set(doxyfile_in ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Doxyfile.in)
-    set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
-
-    configure_file(${doxyfile_in} ${doxyfile} @ONLY)
-
-    add_custom_target(doc ALL
-        COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        COMMENT "Generating documentation with Doxygen"
-        VERBATIM)
-
-	install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html DESTINATION share/doc)
+if(NOT DOXYGEN_FOUND)
+    	message(FATAL_ERROR "Doxygen not found!")
 endif()
+
+set(doxyfile_in ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Doxyfile.in)
+set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+
+configure_file(${doxyfile_in} ${doxyfile} @ONLY)
+
+add_custom_target(doc ALL
+    COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMENT "Generating documentation with Doxygen"
+    VERBATIM)
+
+install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html DESTINATION share/doc)
