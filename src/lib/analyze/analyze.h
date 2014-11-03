2958911c9ee179d133bd6fda6e8ba65c8eccd451
Kasper Steensig <reindeernix@prozum.dk>
2014-11-03 09:38:25 +0100

    edited doxygen documentation for analyze{.c,.h}

--- src/lib/analyze/analyze.h
+++ src/lib/analyze/analyze.h
@@ -3,24 +3,24 @@
 
 #include <mid/mid.h>
 
-/** Struct that contains the node on a single track */
+/** Struct that contains the information of nodes on a single track */
 typedef struct note_s
 {
     uint8_t *notes;    /**< ADD DOXYGEN DOC HERE */
     uint8_t *velocity; /**< ADD DOXYGEN DOC HERE */
 } note_t;
 
-/* Struct that contains an array with all the tracks' nodes */
+/** Struct that contains an array with all the information of nodes for all tracks */
 typedef struct song_s
 {
     note_t *notes_all;    /**< ADD DOXYGEN DOC HERE */
     note_t *velocity_all; /**< ADD DOXYGEN DOC HERE */
 } song_t;
 
-/* Returns all the nodes for all the tracks */
+/** Returns all the nodes for all the tracks */
 song_t *song_extract(mid_t *mid);
 
-/* Returns all the nodes for a single track */
+/** Returns all the nodes for a single track */
 note_t *note_extract(track_t *track, uint32_t event_count, uint32_t track_num);
 
 #endif
