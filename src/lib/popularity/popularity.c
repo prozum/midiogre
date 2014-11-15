edffc21f87cf53c3c122120b1375b4fe613e67de
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2014-11-15 23:25:38 +0100

    * Changed name of variable playCount to plays * Changed name of function absPlayCount to playCount * Changed data type of plays from int to unsigned int * Removed function testTime

--- src/lib/popularity/popularity.c
+++ src/lib/popularity/popularity.c
@@ -8,39 +8,34 @@
 
 #define TIMECONSTANT 45000
 
-/*Print current time in Unix Time in secounds*/
-void testTime(){
-    printf("%d sekunder\ns",getUnixTimeStamp());
-}
-
 /*Get Unix Time, and returns it in seconds*/
 unsigned int getUnixTimeStamp(){
-    return (unsigned)time(NULL);
+    return (unsigned int)time(NULL);
 }
 
 /*Calculates diffrence between the starttime(time of creation) of the song and current time*/
 unsigned int compareTime(unsigned int startTime){
-    return (unsigned)time(NULL)-startTime;
+    return (unsigned int)time(NULL)-startTime;
 }
 
 /*Checks if the song has been played*/
-int isPlayed(int plays){
+int isPlayed(unsigned int plays){
     if(plays>0)
         return 1;
     else
         return 0;
 }
 
-/*Ensures the playcount always is possitive or equal 0*/
-int absPlayCount(int playCount){
-    if(abs(playCount) >= 1)
-        return abs(playCount);
-    else
+/*Prevents the program from taking log10(0)*/
+unsigned int playCount(unsigned int plays){
+    if(plays == 0)
         return 1;
+    else
+        return plays;
 }
 
 /*Calculates the score af the song*/
-/*No matter how old the song is, it wont get any score before it is viewed at least once*/
-double weigth(unsigned int startTime, int playCount){
-    return log10((double)absPlayCount(playCount))+(isPlayed(playCount)*compareTime(startTime))/TIMECONSTANT;
+/*No matter how old the song is, it wont get any weigth before it is viewed more than once, or once and has been around for a longer time*/
+double weigth(unsigned int startTime, int plays){
+    return log10((double)playCount(plays))+(isPlayed(plays)*compareTime(startTime))/TIMECONSTANT;
 }
