ebe1e0edd5b3ba415e7976b26c09c3a25d00dd10
BlizzardofStone <contesta_john@outlook.com>
2014-11-19 11:28:57 +0100

    Typos

--- samples/poptest/poptest.c
+++ samples/poptest/poptest.c
@@ -7,11 +7,12 @@ int main(void){
 
     /* Testing value generated 16/11/2014 kl. 22:55 - 1416174915*/
 
-    unsigned int timestamp = 1416174915; /* This will be the value given to a song upon adding it to Midiogre(c). Please make sure this number is always bigger than BASETIME */
-    unsigned int plays = 0; /* times the song has been played */
+    unsigned int timestamp = 1416174915; /* This will be the value given to a song upon adding it to Midiogre(c). 
+                                             Please make sure this number is always bigger than BASETIME */
+    unsigned int plays = 5000; /* times the song has been played */
 
-    printf("weigth:\n");
-    double score = weigth(timestamp, plays);
+    printf("weight:\n");
+    double score = weight(timestamp, plays);
     printf("%f\n",score);
 
     return 0;
