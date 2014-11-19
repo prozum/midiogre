d90895c45edbf82bb9b04855f2c9378d0ffe95c9
BlizzardofStone <contesta_john@outlook.com>
2014-11-19 11:50:06 +0100

    Start work on pop_balance

---
+++ src/lib/popularity/pop_balance.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Crit: Hvor stor match procent, Hvor mange views (hvor lavere foretrækkes)
+// Skal balanceres så foreslag ikke får for meget fokus på ukendt uden views.
