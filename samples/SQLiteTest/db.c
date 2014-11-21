26d9abd537886a8253af52ec8797bd8bb4ea19e5
karlsmart <the.mark.lauridsen@gmail.com>
2014-11-21 14:36:43 +0100

    Added insert to table in samples/sqlitetest/db.c

--- samples/SQLiteTest/db.c
+++ samples/SQLiteTest/db.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
-#include <mid.h>
+#include <mid/mid.h>
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    int i;
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
    sqlite3 *db;
    char *zErrMsg = 0;
    int  rc;
-   char *sql;
+   char *sql, *sql2, *sql3;
 
    /* Open database */
    rc = sqlite3_open("test.db", &db);
@@ -30,13 +30,22 @@ int main(int argc, char* argv[])
 
    /* Create SQL statement */
    sql = "CREATE TABLE midiFile("  \
-         "ARTIST      CHAR(32)  ," \
-         "Album       CHAR(32)  ," \
-         "Name        CHAR(32)  ," \
-         "Track       INT );";
+         "ARTIST       CHAR(32)," \
+         "Album        CHAR(32)," \
+         "Name         CHAR(32)," \
+         "Track        INT);";
+
+   sql2 = "INSERT INTO midiFile(ARTIST,Album,Name,Track) \
+           VALUES ('BONO','Money For Bono','Raining Kush',1);";
+
+   sql3 = "INSERT INTO midiFile(ARTIST,Album,Name,Track) \
+           VALUES ('BONO2','Money For Bono','More4me',2);";
 
    /* Execute SQL statement */
    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   rc = sqlite3_exec(db, sql2, callback, 0, &zErrMsg);
+   rc = sqlite3_exec(db, sql3, callback, 0, &zErrMsg);
+
    if( rc != SQLITE_OK ){
    fprintf(stderr, "SQL error: %s\n", zErrMsg);
       sqlite3_free(zErrMsg);
