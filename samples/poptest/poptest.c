7efa2e9313c76c7a0e8c4c892607a3af3d18f504
Niclas Moeslund Overby <nimoov@prozum.dk>
2014-11-22 12:46:03 +0100

    * Fix compiler warnings * Combine two printf calls into one

--- samples/poptest/poptest.c
+++ samples/poptest/poptest.c
@@ -4,16 +4,17 @@
 #include <stdlib.h>
 
 int main(void){
-
+    unsigned int timestamp,plays;
+    double score;
+    
     /* Testing value generated 16/11/2014 kl. 22:55 - 1416174915*/
 
-    unsigned int timestamp = 1416174915; /* This will be the value given to a song upon adding it to Midiogre(c). 
+    timestamp = 1416174915; /* This will be the value given to a song upon adding it to Midiogre(c). 
                                              Please make sure this number is always bigger than BASETIME */
-    unsigned int plays = 5000; /* times the song has been played */
-
-    printf("weight:\n");
-    double score = weight(timestamp, plays);
-    printf("%f\n",score);
+    plays = 5000; /* times the song has been played */
+    
+    score = weight(timestamp, plays);
+    printf("weight:\n %f \n",score);
 
     return 0;
 }
