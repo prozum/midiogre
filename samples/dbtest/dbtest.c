b242f4048f5e63678906d845c4851c95db27e638
Niclas Moeslund Overby <nimoov@prozum.dk>
2014-11-24 11:52:17 +0100

    4 spaces please

--- samples/dbtest/dbtest.c
+++ samples/dbtest/dbtest.c
@@ -56,49 +56,49 @@ int main(int argc, char* argv[])
     mid = read_mid(file);
     fclose(file);
 
-   /* Open database */
-   rc = sqlite3_open("test.db", &db);
-   if( rc ){
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      exit(0);
-   }else{
-      fprintf(stdout, "Opened database successfully\n");
-   }
-
-   /* Create SQL statement */
-   sql = "CREATE TABLE midiFile("       \
-         "PARA1        UNSIGNED INT,"   \
-         "PARA2        UNSIGNED INT,"   \
-         "DELTA        UNSIGNED INT);";
-
-   for (i = 0; i < mid->tracks; i++){
-       /* For each event in track */
-       for (j = 0; j < mid->track[i].events; j++) {
-
-           /* If meta message */
-           if (mid->track[i].event[j].msg >= NOTE_ON_1 &&
-               mid->track[i].event[j].msg <= NOTE_ON_16) {
-
-               asprintf(&sql2,"INSERT INTO midiFile(PARA1,PARA2,DELTA) \
-                               VALUES (%u, %u, %u);",mid->track[i].event[j].para_1, \
-                                                     mid->track[i].event[j].para_2, \
-                                                     mid->track[i].event[j].delta);
-               rc = sqlite3_exec(db, sql2, callback, 0, &zErrMsg);
-               free(sql2);
-           }
-       }
-   }
-
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-
-   if( rc != SQLITE_OK ){
-   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-   sqlite3_free(zErrMsg);
-   }else{
-      fprintf(stdout, "Table created successfully\n");
-   }
-   sqlite3_close(db);
-
-   return 0;
+    /* Open database */
+    rc = sqlite3_open("test.db", &db);
+    if( rc ){
+       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+       exit(0);
+    }else{
+       fprintf(stdout, "Opened database successfully\n");
+    }
+ 
+    /* Create SQL statement */
+    sql = "CREATE TABLE midiFile("       \
+          "PARA1        UNSIGNED INT,"   \
+          "PARA2        UNSIGNED INT,"   \
+          "DELTA        UNSIGNED INT);";
+ 
+    for (i = 0; i < mid->tracks; i++){
+        /* For each event in track */
+        for (j = 0; j < mid->track[i].events; j++) {
+ 
+            /* If meta message */
+            if (mid->track[i].event[j].msg >= NOTE_ON_1 &&
+                mid->track[i].event[j].msg <= NOTE_ON_16) {
+ 
+                asprintf(&sql2,"INSERT INTO midiFile(PARA1,PARA2,DELTA) \
+                                VALUES (%u, %u, %u);",mid->track[i].event[j].para_1, \
+                                                      mid->track[i].event[j].para_2, \
+                                                      mid->track[i].event[j].delta);
+                rc = sqlite3_exec(db, sql2, callback, 0, &zErrMsg);
+                free(sql2);
+            }
+        }
+    }
+ 
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+ 
+    if( rc != SQLITE_OK ){
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+    }else{
+       fprintf(stdout, "Table created successfully\n");
+    }
+    sqlite3_close(db);
+ 
+    return 0;
 }
