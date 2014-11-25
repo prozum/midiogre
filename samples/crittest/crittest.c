49f8c11b8aa18febd5706633b3a675ab30b306e3
Fixdonu <jonathanmagnussen@gmail.com>
2014-11-25 09:51:18 +0100

    Revert "Mere crittest"
    This reverts commit 2240b0253b8f056fa00face18f897cd8ce19559d.

--- samples/crittest/crittest.c
+++ samples/crittest/crittest.c
@@ -1,42 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <mid/mid.h>
-#include <mid/mid-util.h>
+#include "analyze.h"
 
-int main(int argc, char* argv[])
-{
-    FILE *mid_file;
-    mid_t *mid;
-    int i,j,k;
+void instruments(mid_t *mid){
+  unsigned int i, j, k;
+}
 
-    /* Open file */
-    mid_file = fopen(argv[1], "rb");
-    if( mid_file == NULL ) {
-        perror(argv[1]);
-        return -1;
-    }
-    /* Read mid */
-    mid = read_mid(mid_file);
-
-    for (i = 0; i < mid->tracks; i++){
-        /* For each event in track */
-        for (j = 0; j < mid->track[i].events; j++) {
-
-            /* If progream change */
-            if (mid->track[i].event[j].msg >= PRG_CHANGE_1 &&
-                mid->track[i].event[j].msg <= PRG_CHANGE_16) {
-
-                if (mid->track[i].events[j].para_1 >= 7) {
-                    printf("piano");
-                }
-                else if (mid->track[i].events[j].para_1 >= 15) {
-                printf("Chromatic Percussion");
-                }
-            }
-
-    /* Close mid_file */
-    fclose(mid_file);
-    printf("\n");
-    return 0;
+int main (void){
+  printf("hello");
 }
