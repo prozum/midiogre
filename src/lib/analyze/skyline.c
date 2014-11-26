1162a0eae022cf27e1238ac13f70edea7a4a596a
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2014-11-26 15:30:47 +0100

    Started implementing new version of the skyline algorithm.

--- src/lib/analyze/skyline.c
+++ src/lib/analyze/skyline.c
@@ -5,72 +5,62 @@
 #include "analyze.h"
 #include "skyline.h"
 
-/*testcode*/
-
 channel_t skyline(song_data_t song_data){
     unsigned int i, j, k;
 
-    /* Creates a result array to carry the notes that are not eliminated */
+    /* Creates a skyline_dynamic array to carry the notes that are not eliminated */
 
-    channel_t *result = (channel_t *)malloc(sizeof(channel_t) * CHANNELS);
+    channel_t *skyline_dynamic = (channel_t *)malloc(sizeof(channel_t) * CHANNELS);
 
-    result->notes = NULL;
+    skyline_dynamic->notes = NULL;
 
     for (i = 0; i < CHANNELS; i++) {
-        result[i].notes = malloc( sizeof( note_t ) * song_data.channels[i].channel_length );
-        result[i].channel_length = 0;
+        skyline_dynamic[i].notes = malloc( sizeof( note_t ) * song_data.channels[i].channel_length );
+        skyline_dynamic[i].channel_length = 0;
     }
 
-    /* Copies data from song_data to result */
+    /* Copies data from song_data to skyline_dynamic */
 
     for(i = 0; i < CHANNELS; i++){
         for(j = 0; j < song_data.channels[i].channel_length; j++){
-            result[i].notes[j].pitch = song_data.channels[i].notes[j].pitch;
-            result[i].notes[j].onset = song_data.channels[i].notes[j].onset;
-            result[i].notes[j].offset = song_data.channels[i].notes[j].offset;
+            skyline_dynamic[i].notes[j].pitch = song_data.channels[i].notes[j].pitch;
+            skyline_dynamic[i].notes[j].onset = song_data.channels[i].notes[j].onset;
+            skyline_dynamic[i].notes[j].offset = song_data.channels[i].notes[j].offset;
         }
     }
 
-    /* Skyline algorithm */
+    for(i = 0; i < CHANNELS; i++) {
+        for(j = 0; j < song_data.channels[i].channel_length; j++) {
+            k = j + 1;
 
-    for(i = 0, j = 0; i < CHANNELS; i++){
-        k = j + 1;
-
-        while(song_data.channels[i].notes[j].onset == song_data.channels[i].notes[k].onset && j < song_data.channels[i].channel_length){
-            if(song_data.channels[i].notes[j].pitch < song_data.channels[i].notes[k].onset){
-                result[i].notes[j].pitch = song_data.channels[i].notes[k].pitch;
-                /* song_data.channels[i].notes[j].pitch = 0; */
-                j = k;
-            }else{
-                result[i].notes[k].pitch = song_data.channels[i].notes[j].pitch;
-                /* song_data.channels[i].notes[k].pitch = 0; */
-                k++;
+            if(song_data.channels[i].notes[j].onset == song_data.channels[i].notes[k].onset) {
+                if(song_data.channels[i].notes[j].pitch > song_data.channels[i].notes[k].pitch) {
+                    song_data.channels[i].notes[k].onset = song_data.channels[i].notes[j].offset;
+                }
+                if (song_data.channels[i].notes[j].pitch < song_data.channels[i].notes[k].pitch) {
+                    song_data.channels[i].notes[j].onset = song_data.channels[i].notes[k].offset;
+                }
             }
 
-            if(song_data.channels[i].notes[j].offset > song_data.channels[i].notes[j].offset){
-                result[i].notes[j].offset = song_data.channels[i].notes[k].onset;
-            }
-        }
-
-        j = k;
-    }
-
-    /* Compare results with the original file */
-
-    for(i = 0; i < CHANNELS; i++){
-        for(j = 0; j < result[i].channel_length; j++){
-            if(song_data.channels[i].notes[j].pitch != result[i].notes[j].pitch){
-                printf("Channel %d note %d pitch is different! Pitch is %d vs %d\n", i, j, song_data.channels[i].notes[j].pitch, result[i].notes[j].pitch);
+            if(song_data.channels[i].notes[j].offset > song_data.channels[i].notes[k].onset) {
+                if(song_data.channels[i].notes[j].pitch > song_data.channels[i].notes[k].pitch) {
+                    song_data.channels[i].notes[k].onset = song_data.channels[i].notes[j].offset;
+                }
+                if(song_data.channels[i].notes[j].pitch < song_data.channels[i].notes[k].pitch) {
+                    song_data.channels[i].notes[j].offset = song_data.channels[i].notes[k].onset;
+                }
             }
 
-            if(song_data.channels[i].notes[j].onset != result[i].notes[j].onset){
-                printf("Channel %d note %d onset is different! Onset is %d vs %d\n", i, j, song_data.channels[i].notes[j].onset, result[i].notes[j].onset);
+            if(song_data.channels[i].notes[j].offset > song_data.channels[i].notes[k].offset) {
+                if(song_data.channels[i].notes[j].pitch > song_data.channels[i].notes[k].pitch) {
+                    /* Eliminate nk */
+                }
+                if(song_data.channels[i].notes[j].pitch < song_data.channels[i].notes[k].pitch) {
+                    /* Split */
+                }
             }
-        }
-        if(song_data.channels[i].notes[j].offset != result[i].notes[j].offset){
-            printf("Channel %d note %d offset is different! Offset is %d vs %d\n", i, j, song_data.channels[i].notes[j].offset, result[i].notes[j].offset);
         }
     }
 
-    return *result;
+    return *skyline_dynamic;
 }
