082d356e89e8ffd839f5bda6c32433b0f20b12f6
Niclas Moeslund Overby <nimoov@prozum.dk>
2014-11-26 19:57:18 +0100

    dbtest: remove useless if statement + cleanup

--- samples/dbtest/dbtest.c
+++ samples/dbtest/dbtest.c
@@ -47,13 +47,14 @@ int midiWrite(mid_t *mid){
 
 int main(int argc, char* argv[])
 {
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int  rc;
-    char *sql, *sql2;
-    FILE *file;
     mid_t *mid;
+    
+    sqlite3 *db;
+    
+    int  rc;
     unsigned int i,j;
+    char *sql, *sql2*, error = 0;
+    FILE *file;
 
     /* Open mid file */
     file = fopen(argv[1],"rb");
@@ -70,61 +71,57 @@ int main(int argc, char* argv[])
     rc = sqlite3_open("test.db", &db);
     if( rc ){
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        exit(0);
+        return -1;
     } else {
         fprintf(stdout, "Opened database successfully\n");
     }
     
     /* Write database structure */
-    if (rc != 1){
-        /* Create SQL statement */
-        sql = "CREATE TABLE midiFile("       \
-              "PARA1        UNSIGNED INT,"   \
-              "PARA2        UNSIGNED INT,"   \
-              "DELTA        UNSIGNED INT);";
-     
-        /* Execute SQL statement */
-        rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+    sql = "CREATE TABLE midiFile("       \
+          "PARA1        UNSIGNED INT,"   \
+          "PARA2        UNSIGNED INT,"   \
+          "DELTA        UNSIGNED INT);";
+    
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &error);
 
-        if (rc != SQLITE_OK){
-            fprintf(stderr, "SQL error: %s\n", zErrMsg);
-            sqlite3_free(zErrMsg);
-        } else {
-            fprintf(stdout, "Table created successfully\n");
-        }
+    if (rc != SQLITE_OK){
+        fprintf(stderr, "SQL error: %s\n", error);
+        sqlite3_free(error);
+    } else {
+        fprintf(stdout, "Table created successfully\n");
+    }
 
-        /* For each track in mid */
-        for (i = 0; i < mid->tracks; i++){
-            /* For each event in track */
-            for (j = 0; j < mid->track[i].events; j++) {
-     
-                /* If meta message */
-                if (mid->track[i].event[j].msg >= NOTE_ON_1 &&
-                    mid->track[i].event[j].msg <= NOTE_ON_16) {
-     
-                    asprintf(&sql2, "INSERT INTO midiFile(PARA1,PARA2,DELTA) \
-                                    VALUES (%u, %u, %u);",mid->track[i].event[j].para_1, \
-                                                          mid->track[i].event[j].para_2, \
-                                                          mid->track[i].event[j].delta);
-                    
-                    rc = sqlite3_exec(db, sql2, callback, 0, &zErrMsg);
-                    
-                    if (rc != SQLITE_OK) {
-                        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-                        sqlite3_free(zErrMsg);
-                    } else {
-                        fprintf(stdout, "Data inserted successfully\n");
-                    }
-                    
-                    free(sql2)
-                    
+    /* For each track in mid */
+    for (i = 0; i < mid->tracks; i++) {
+        /* For each event in track */
+        for (j = 0; j < mid->track[i].events; j++) {
+    
+            /* If meta message */
+            if (mid->track[i].event[j].msg >= NOTE_ON_1 &&
+                mid->track[i].event[j].msg <= NOTE_ON_16) {
+    
+                asprintf(&sql2, "INSERT INTO midiFile(PARA1,PARA2,DELTA) \
+                                VALUES (%u, %u, %u);",mid->track[i].event[j].para_1, \
+                                                      mid->track[i].event[j].para_2, \
+                                                      mid->track[i].event[j].delta);
+                
+                rc = sqlite3_exec(db, sql2, callback, 0, &error);
+                
+                if (rc != SQLITE_OK) {
+                    fprintf(stderr, "SQL error: %s\n", error);
+                    sqlite3_free(error);
+                } else {
+                    fprintf(stdout, "Data inserted successfully\n");
                 }
+                
+                free(sql2)
+                
             }
         }
-     
-
-        sqlite3_close(db);
     }
+    
+    sqlite3_close(db);    
  
     return 0;
 }
