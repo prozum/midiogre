9cbf7da393d5a8fba768fd8f90d40c9b4d9a0450
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2014-11-27 10:06:25 +0100

    Fixed skyline.c to use note and notes instead of notes and channel_length. Program should now compile correctly.

--- src/lib/analyze/skyline.c
+++ src/lib/analyze/skyline.c
@@ -12,50 +12,50 @@ channel_t skyline(song_data_t song_data){
 
     channel_t *skyline_dynamic = (channel_t *)malloc(sizeof(channel_t) * CHANNELS);
 
-    skyline_dynamic->notes = NULL;
+    skyline_dynamic->note = NULL;
 
     for (i = 0; i < CHANNELS; i++) {
-        skyline_dynamic[i].notes = malloc( sizeof( note_t ) * song_data.channels[i].channel_length );
-        skyline_dynamic[i].channel_length = 0;
+        skyline_dynamic[i].note = malloc( sizeof( note_t ) * song_data.channels[i].notes );
+        skyline_dynamic[i].notes = 0;
     }
 
     /* Copies data from song_data to skyline_dynamic */
 
     for(i = 0; i < CHANNELS; i++){
-        for(j = 0; j < song_data.channels[i].channel_length; j++){
-            skyline_dynamic[i].notes[j].pitch = song_data.channels[i].notes[j].pitch;
-            skyline_dynamic[i].notes[j].onset = song_data.channels[i].notes[j].onset;
-            skyline_dynamic[i].notes[j].offset = song_data.channels[i].notes[j].offset;
+        for(j = 0; j < song_data.channels[i].notes; j++){
+            skyline_dynamic[i].note[j].pitch = song_data.channels[i].note[j].pitch;
+            skyline_dynamic[i].note[j].onset = song_data.channels[i].note[j].onset;
+            skyline_dynamic[i].note[j].offset = song_data.channels[i].note[j].offset;
         }
     }
 
     for(i = 0; i < CHANNELS; i++) {
-        for(j = 0; j < song_data.channels[i].channel_length; j++) {
+        for(j = 0; j < song_data.channels[i].notes; j++) {
             k = j + 1;
 
-            if(song_data.channels[i].notes[j].onset == song_data.channels[i].notes[k].onset) {
-                if(song_data.channels[i].notes[j].pitch > song_data.channels[i].notes[k].pitch) {
-                    song_data.channels[i].notes[k].onset = song_data.channels[i].notes[j].offset;
+            if(song_data.channels[i].note[j].onset == song_data.channels[i].note[k].onset) {
+                if(song_data.channels[i].note[j].pitch > song_data.channels[i].note[k].pitch) {
+                    song_data.channels[i].note[k].onset = song_data.channels[i].note[j].offset;
                 }
-                if (song_data.channels[i].notes[j].pitch < song_data.channels[i].notes[k].pitch) {
-                    song_data.channels[i].notes[j].onset = song_data.channels[i].notes[k].offset;
-                }
-            }
-
-            if(song_data.channels[i].notes[j].offset > song_data.channels[i].notes[k].onset) {
-                if(song_data.channels[i].notes[j].pitch > song_data.channels[i].notes[k].pitch) {
-                    song_data.channels[i].notes[k].onset = song_data.channels[i].notes[j].offset;
-                }
-                if(song_data.channels[i].notes[j].pitch < song_data.channels[i].notes[k].pitch) {
-                    song_data.channels[i].notes[j].offset = song_data.channels[i].notes[k].onset;
+                if (song_data.channels[i].note[j].pitch < song_data.channels[i].note[k].pitch) {
+                    song_data.channels[i].note[j].onset = song_data.channels[i].note[k].offset;
                 }
             }
 
-            if(song_data.channels[i].notes[j].offset > song_data.channels[i].notes[k].offset) {
-                if(song_data.channels[i].notes[j].pitch > song_data.channels[i].notes[k].pitch) {
+            if(song_data.channels[i].note[j].offset > song_data.channels[i].note[k].onset) {
+                if(song_data.channels[i].note[j].pitch > song_data.channels[i].note[k].pitch) {
+                    song_data.channels[i].note[k].onset = song_data.channels[i].note[j].offset;
+                }
+                if(song_data.channels[i].note[j].pitch < song_data.channels[i].note[k].pitch) {
+                    song_data.channels[i].note[j].offset = song_data.channels[i].note[k].onset;
+                }
+            }
+
+            if(song_data.channels[i].note[j].offset > song_data.channels[i].note[k].offset) {
+                if(song_data.channels[i].note[j].pitch > song_data.channels[i].note[k].pitch) {
                     /* Eliminate nk */
                 }
-                if(song_data.channels[i].notes[j].pitch < song_data.channels[i].notes[k].pitch) {
+                if(song_data.channels[i].note[j].pitch < song_data.channels[i].note[k].pitch) {
                     /* Split */
                 }
             }
