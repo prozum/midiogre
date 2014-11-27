ce2e77d5e25948b05ff13d78a78cdaac07442a8f
BlizzardofStone <contesta_john@outlook.com>
2014-11-27 10:32:14 +0100

    DB RC

--- samples/dbtest/dbtest.c
+++ samples/dbtest/dbtest.c
@@ -6,22 +6,13 @@
 #include <mid/mid.h>
 #include <mid/mid-str.h>
 
-#include <mid/db.c>
-#include <mid/db.h>
+#include <database/db.c>
+#include <database/db.h>
 
 #ifdef _WIN32
 #include <win/asprintf.h>
 #endif
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName)
-{
-   int i;
-   for(i=0; i<argc; i++){
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
 
 int main(int argc, char* argv[])
 {
--- src/lib/database/db.c
+++ src/lib/database/db.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "db.h"
 
 #include <sqlite3.h>
 
