63e0d3123279894ce4489178b7bb4bde4831b12e
BlizzardofStone <contesta_john@outlook.com>
2014-11-27 11:47:33 +0100

    DB

--- src/lib/database/db.c
+++ src/lib/database/db.c
@@ -13,3 +13,13 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName)
    printf("\n");
    return 0;
 }
+
+int database_open_error (int rc, sqlite3 *db){
+    if(rc){
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return -1;
+    } else {
+        fprintf(stdout, "Opened database successfully\n");
+    }
+    return 0;
+}
