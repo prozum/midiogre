ec45c85657444ba90260a0014d6f75f2fa234c3f
Niclas Moeslund Overby <nimoov@prozum.dk>
2014-12-08 10:16:10 +0100

    midiogre: fix compile

--- src/midiogre-mid.c
+++ src/midiogre-mid.c
@@ -9,17 +9,19 @@
 
 #include <gtk/gtk.h>
 #include <glib.h>
+#include <glib/gprintf.h>
 
 /** Import mid struct to tree view */
 void mid_import(GFile *mid_gfile, GtkWidget *notebook)
 {
     uint32_t i, j, k;
     uint32_t len;
-    char tmp[3], *data_str[NUM_COLUMNS];
+    char tmp[3];
+    char *data_str[NUM_COLUMNS];
 
     FILE *mid_file;
     mid_t *mid;
-    
+
     GtkWidget *treeview,*sw;
     GtkTreeModel *model;
     GtkListStore *store;
@@ -30,10 +32,9 @@ void mid_import(GFile *mid_gfile, GtkWidget *notebook)
 
     /* Open mid file */
     if((mid_file = fopen(g_file_get_path(mid_gfile),"rb")) == NULL) {
-        perror(argv[1]);
+        perror(g_file_get_path(mid_gfile));
         return -1;
     }
-
     /* Read content */
     mid = read_mid(mid_file);
     fclose(mid_file);
@@ -55,44 +56,37 @@ void mid_import(GFile *mid_gfile, GtkWidget *notebook)
 
             /* Convert data to hex */
             data_str[COLUMN_MSG] = g_strdup_printf("%x",mid->track[i].event[j].msg);
-            data_str[COLUMN_PARA_1] = g_strdup_printf("%x",mid->track[i].event[j].para_1);
-            data_str[COLUMN_PARA_2] = g_strdup_printf("%x",mid->track[i].event[j].para_2);
+            data_str[COLUMN_BYTE_1] = g_strdup_printf("%x",mid->track[i].event[j].byte_1);
+            data_str[COLUMN_BYTE_2] = g_strdup_printf("%x",mid->track[i].event[j].byte_2);
             data_str[COLUMN_DELTA] = g_strdup_printf("%x",mid->track[i].event[j].delta);
-            
-            /* If channel message */ 
+
+            /* If channel message */
             if (mid->track[i].event[j].msg >= NOTE_OFF &&
-                mid->track[i].event[j].msg <= PITCH_BEND) { 
-                
+                mid->track[i].event[j].msg <= PITCH_BEND) {
+
                 data_str[COLUMN_CHAN] = g_strdup_printf("%x",mid->track[i].event[j].chan);
             } else {
                 data_str[COLUMN_CHAN] = g_strdup_printf("N/A");
             }
 
-            
+
             /* If meta message */
             if (mid->track[i].event[j].msg == META_MSG) {
 
                 /* Find string length */
-                len = mid->track[i].event[j].para_2 * 3;
+                len = mid->track[i].event[j].byte_2 * 3;
 
                 data_str[COLUMN_DATA] = (char *)g_malloc((len + 1) * sizeof(char));
                 strcpy(data_str[COLUMN_DATA],"");
 
                 /* For each byte in meta data */
-                for (k = 0; k < mid->track[i].event[j].para_2; k++) {
+                for (k = 0; k < mid->track[i].event[j].byte_2; k++) {
 
                     /* Convert Data to hex */
-<<<<<<< HEAD
-                    if (mid->track[i].event[j].mdata[k] > 0xF) {
-                        g_sprintf(&tmp,"%x ",mid->track[i].event[j].mdata[k]);
-                    } else {
-                        g_sprintf(&tmp,"0%x ",mid->track[i].event[j].mdata[k]);
-=======
                     if (mid->track[i].event[j].data[k] > 0xF) {
-                        g_sprintf(&tmp,"%x ",mid->track[i].event[j].data[k]);
+                        g_sprintf(tmp,"%x ",mid->track[i].event[j].data[k]);
                     } else {
-                        g_sprintf(&tmp,"0%x ",mid->track[i].event[j].data[k]);
->>>>>>> parent of de18f06... libanalyze: cleanup
+                        g_sprintf(tmp,"0%x ",mid->track[i].event[j].data[k]);
                     }
                     strcat(data_str[COLUMN_DATA],tmp);
                 }
@@ -105,8 +99,8 @@ void mid_import(GFile *mid_gfile, GtkWidget *notebook)
             gtk_list_store_append(store, &iter);
             gtk_list_store_set(store, &iter,
                                COLUMN_MSG,    data_str[COLUMN_MSG],
-                               COLUMN_PARA_1, data_str[COLUMN_PARA_1],
-                               COLUMN_PARA_2, data_str[COLUMN_PARA_2],
+                               COLUMN_BYTE_1, data_str[COLUMN_BYTE_1],
+                               COLUMN_BYTE_2, data_str[COLUMN_BYTE_2],
                                COLUMN_CHAN,   data_str[COLUMN_CHAN],
                                COLUMN_DELTA,  data_str[COLUMN_DELTA],
                                COLUMN_DATA,   data_str[COLUMN_DATA],
@@ -136,20 +130,20 @@ void mid_import(GFile *mid_gfile, GtkWidget *notebook)
 
         /* Para_1 column */
         renderer = gtk_cell_renderer_text_new();
-        column = gtk_tree_view_column_new_with_attributes("Para 1",
+        column = gtk_tree_view_column_new_with_attributes("Byte 1",
                                                           renderer,
                                                           "text",
-                                                          COLUMN_PARA_1,
+                                                          COLUMN_BYTE_1,
                                                            NULL);
 
         gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), column);
 
         /* Para_2 column */
         renderer = gtk_cell_renderer_text_new();
-        column = gtk_tree_view_column_new_with_attributes("Para 2",
+        column = gtk_tree_view_column_new_with_attributes("Byte 2",
                                                           renderer,
                                                           "text",
-                                                          COLUMN_PARA_2,
+                                                          COLUMN_BYTE_2,
                                                           NULL);
 
         gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), column);
@@ -163,7 +157,7 @@ void mid_import(GFile *mid_gfile, GtkWidget *notebook)
                                                           NULL);
 
         gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), column);
-       
+
         /* Delta column */
         renderer = gtk_cell_renderer_text_new();
         column = gtk_tree_view_column_new_with_attributes("Delta Time",
