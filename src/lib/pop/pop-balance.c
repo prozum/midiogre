9f876628cd761ef0ba4d7a999b9d6cb36e2ba1cc
Niclas Moeslund Overby <nimoov@prozum.dk>
2014-12-10 21:19:53 +0100

    libpop: Enable to remove pop-balance.h

--- src/lib/pop/pop-balance.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-// Crit: Hvor stor match procent, Hvor mange views (hvor lavere foretrækkes)
-// Skal balanceres så foreslag ikke får for meget fokus på ukendt uden views.
