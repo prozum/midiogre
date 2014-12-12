48f1baca21887ceadc5f001850511f6046b52860
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2014-12-12 10:21:43 +0100

    * Minor changes to pop. * Added compare function for pop

--- src/lib/pop/pop.c
+++ src/lib/pop/pop.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <time.h>
 
-#define MAX_VIEWS 10000
+#define MAX_PLAYS 10000
 #define MAX_FINGERPPRINT 10
 
 unsigned int calcAge(unsigned int upload_time){
@@ -13,31 +13,50 @@ unsigned int calcAge(unsigned int upload_time){
 }
 
 /* The type for fingerprint may be changed based on the type given to it */
-double song_score(unsigned int views, unsigned int upload_time, unsigned int min_view, unsigned int max_view, unsigned int fingerprint) {
-    unsigned int age = 0, view_rating = 0;
+double song_score(unsigned int plays, unsigned int upload_time, unsigned int min_play, unsigned int max_play, unsigned int fingerprint) {
+    unsigned int age = 0, play_rating = 0;
     double score = 0;
 
-    if(max_view > MAX_VIEWS) {
-        max_view = MAX_VIEWS;
+    if(max_play > MAX_PLAYS) {
+        max_play = MAX_PLAYS;
     }
 
     /* Returns a score of 0 if the song does not meet the required conditions */
-    if(views < min_view) {
+    if(plays < min_play) {
         return 0;
     } else if(fingerprint > MAX_FINGERPPRINT) {
         return 0;
     } else if(fingerprint == 0) {
         return 0;
-    } else if(views > MAX_VIEWS) {
+    } else if(plays > MAX_PLAYS) {
         return 0;
     }
 
     age = calcAge(upload_time);
 
-    view_rating = max_view - views;
+    play_rating = max_play - plays;
 
-    score = ((view_rating)*((21-fingerprint)/log2(fingerprint+1))/log2((double)age))/100;
+    score = ((play_rating)*((21-fingerprint)/log2(fingerprint+1))/log2((double)age))/100;
 
     return score;
 
+}
+
+int compare_song(const void *s1, const void *s2) {
+
+    const song_t *song1 = s1;
+    const song_t *song2 = s2;
+
+    double song1_score = 0, song2_score = 0;
+
+    song1_score = song_score(song1->Plays,song1->TimeAdded,0,MAX_PLAYS,0); /* Argument 5 needed */
+    song2_score = song_score(song2->Plays,song2->TimeAdded,0,MAX_PLAYS,0); /* Argument 5 needed */
+
+    if(song1_score > song2_score) {
+        return 1;
+    } else if(song2_score < song2_score) {
+        return -1;
+    } else {
+        return 0;
+    }
 }
--- src/lib/pop/pop.h
+++ src/lib/pop/pop.h
@@ -3,12 +3,26 @@
 #ifndef _POPULARITY_H_
 #define _POPULARITY_H_
 
+#include <stdint.h>
+
 #define TIMECONSTANT 45000
 #define BASETIME 1416174518 /*Added 22:48 16/11/2014*/
 
 /** Alt time calculator based on the actual model used på reddit as found here: amix.dk/blog/post/19588*/
 unsigned int calcAge(unsigned int upload_time);
 
-double song_score(unsigned int views, unsigned int upload_time, unsigned int min_view, unsigned int max_view, unsigned int fingerprint);
+double song_score(unsigned int plays, unsigned int upload_time, unsigned int min_play, unsigned int max_play, unsigned int fingerprint);
+int compare_score(const void *s1, const void *s2);
+
+typedef struct {
+    char Artist[64];
+    char Album[64];
+    char Title[64];
+    uint32_t TrackNum;
+    uint32_t Length;
+    uint32_t Plays;
+    uint8_t Fingerprint[21];
+    uint32_t TimeAdded;
+} song_t;
 
 #endif
