6b8f17a2098bb0b0e67cc01c91c030e117441762
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2014-12-13 17:48:01 +0100

    Updated the ranking algorithm according to the new fingerprint values.

--- samples/pop-test/pop-test.c
+++ samples/pop-test/pop-test.c
@@ -5,26 +5,19 @@
 #include <time.h>
 
 int main(void){
-    unsigned int timestamp,plays, min_p, max_p, fingerprint = 0;
-    double score;
-    int i = 0, j = 0, f = 0;
-    /* Testing value generated 16/11/2014 kl. 22:55 - 1416174915*/
-
-    timestamp = time(NULL); /* This will be the value given to a song upon adding it to Midiogre(c).
-                                             Please make sure this number is always bigger than BASETIME */
-    plays = 5000; /* times the song has been played */
+    unsigned int min_p, max_p;
+    int i = 0, j = 0;
 
     min_p = 0;
-
     max_p = 10000;
 
 
-    printf("finger\\plays & 0 & 100 & 200 & 300 & 400 & 500 & 600 & 700 & 800s & 900 & 1000 \\\\ \\hline\n");
-    for(i = 0; i <= 10; i++) {
+    printf("finger\\textbackslash plays & 0 & 1000 & 2000 & 3000 & 4000 & 5000 & 6000 & 7000 & 8000 & 9000 & 10000 \\\\ \\hline\n");
+    for(i = 0; i <= 18; i++) {
         printf("%d & ", i);
-        for(j = 0; j <= 1000; j += 100) {
-            printf("%.1f",song_score(j, timestamp - 60, min_p, max_p, i));
-            if(j != 1000) {
+        for(j = 0; j <= 10000; j += 1000) {
+            printf("%.1f",song_score(j, time(NULL) - 3600, min_p, max_p, i));
+            if(j != 10000) {
                 printf(" & ");
             } else {
                 printf(" \\\\ \\hline");
