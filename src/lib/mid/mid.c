608acd1aa7b45866af2b72212adfaf1c21f53996
Niclas Moeslund Overby <nimoov@prozum.dk>
2014-12-16 12:51:39 +0100

    libmid: fix regression

--- src/lib/mid/mid.c
+++ src/lib/mid/mid.c
@@ -219,7 +219,6 @@ int read_events(list_t *data, uint16_t division, uint32_t start_tempo,list_t *ev
         event->time = time_last + event->delta * (tempo/division)/1000;
         time_last = event->time;
 
-    
         /* Get message */
         tmp = list_get(data);
 
@@ -263,7 +262,7 @@ int read_events(list_t *data, uint16_t division, uint32_t start_tempo,list_t *ev
                 event->byte_1 = list_get(data);
 
                 /* Byte 2: Meta length  */
-                if ((get_tmp = list_get(data)) > 0) {
+                if ((get_tmp = list_get(data)) >= 0) {
 
                     event->byte_2 = get_tmp;
 
