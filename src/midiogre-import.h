0dcaf3057e14d68a9ab3961bb443bf16fb2cc1ca
karlsmart <the.mark.lauridsen@gmail.com>
2014-12-18 07:47:29 +0100

    Adding doc to midiogre-import.h. Still needs discriptions

--- src/midiogre-import.h
+++ src/midiogre-import.h
@@ -17,12 +17,40 @@ typedef struct
     GtkProgressBar *progress_bar; /**< Progress bar widget */
 } ImportStatus;
 
+/**
+ * @brief mid_import
+ * @param s:
+ * @return
+ */
 gpointer mid_import(gpointer s);
+
+/**
+ * @brief folder_handler
+ * @param folder_addr
+ * @param mid_addrs
+ * @return
+ */
 int folder_handler(char* folder_addr, GQueue *mid_addrs);
+
+/**
+ * @brief progress_dialog_update
+ * @param s
+ * @return
+ */
 gboolean progress_dialog_update(gpointer s);
 
+/**
+ * @brief import_dialog
+ * @param window
+ * @param queue
+ * @return
+ */
 ImportStatus *import_dialog(GtkWindow *window, GQueue *queue);
-void folder_chooser(GtkWindow *window);
 
+/**
+ * @brief folder_chooser
+ * @param window
+ */
+void folder_chooser(GtkWindow *window);
 
 #endif
