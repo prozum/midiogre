afce76612a57426912f0d04929ee0fa12b359e66
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2014-12-18 05:40:51 +0100

    Added extended documentation to pop.h

--- src/lib/pop/pop.h
+++ src/lib/pop/pop.h
@@ -10,16 +10,32 @@
 #define MAX_EDITDIST 10
 #define MAX_PLAYS 10000
 
-/** Gets the difference in time between upload time and current time*/
+/** Gets the difference in time between upload time and current time
+ * @param upload_time: The time which the file was uploaded to db in unix time.
+ * @return
+ */
 unsigned int delta_time(unsigned int upload_time);
 
-/** Calculates the ranking score using all three parameters */
-double song_score_all(unsigned int plays, unsigned int upload_time, uint8_t fingerprint);
+/** Calculates the ranking score using all three parameters
+ * @param plays: The number of plays for the song
+ * @param upload_time: The time which the file was uploaded to db in unix time
+ * @param edit_dist: the edit distance for the song
+ * @return
+ */
+double song_score_all(unsigned int plays, unsigned int upload_time, uint8_t edit_dist);
 
-/** Calculates the ranking score using only plays and upload time */
+/** Calculates the ranking score using only plays and upload time
+ * @param plays: The number of plays for the song
+ * @param upload_time: The time which the file was uploaded to db in unix time
+ * @return
+ */
 double song_score_play(unsigned int plays, unsigned int upload_time);
 
-/** Compare function used when comparing two midi files for song_score_play */
+/** Compare function used when comparing two midi files for song_score_play
+ * @param p1: Pointer to song 1
+ * @param p2: Pointer to song 2
+ * @return
+ */
 int song_compare_pop(const void *p1, const void *p2);
 
 #endif
